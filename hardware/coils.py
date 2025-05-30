56458fa45cf39f78a290c3c47df6aa0579a0957f
Noel Woods <noel.woods@uon.edu.au>
2025-05-30 17:25:54 +1000

    Working conversion between PWM, Current and Field Strength. Verify Matrices

--- hardware/coils.py
+++ hardware/coils.py
@@ -20,33 +20,50 @@ class Coils:
         #self.PWM = [PWMOutputDevice(pin, frequency=PWM_FREQUENCY) for pin in self.PWM_PINS]
         self.DIR = [OutputDevice(pin) for pin in self.PIN_DIR]
 
+        self.M1 = np.array([0.1047, 0.1111, 0.1500, 0.1579, 0.2195, 0.2250])
+        self.M1 = np.diag(self.M1)
+        self.b1 = np.array([1, 1, 1, 1, 1, 1])
+
+        self.M2 = np.array([0.1047, 0.1111, 0.1500, 0.1579, 0.2195, 0.2250])
+        self.M2 = np.diag(self.M2)
+        self.b2 = np.array([0.1489, 0.2567, -0.1000, -0.1667, 0.0000, -0.0556])
+        
+        self.M3 = self.M2 @ self.M1
+        self.b3 = self.M2 @ self.b1 + self.b2
+
     def pwm_to_current(self, PWM):
-        M = np.array([0.1047, 0.1111, 0.1500, 0.1579, 0.2195, 0.2250])
-        M = np.diag(M)
-        b = np.array([1, 1, 1, 1, 1, 1])
-        I = M @ PWM + b
-        print(I)
-        return I
+        I = self.M1 @ PWM + self.b1
+        return I # Current [A]
 
-    """
-    def current_to_field(I):
-        return B
+    def current_to_field(self, I):
+        B = self.M2 @ I + self.b2
+        return B # Field Strength [mT]
 
-    def pwm_to_field(PWM):
-        return B
+    def pwm_to_field(self, PWM):
+        B = self.M3 @ PWM + self.b3
+        return B # Field Strength [mT]
 
-    def field_to_current(PWM):
-        return I
+    def field_to_current(self, B):
+        I = np.linalg.inv(self.M2) @ (B - self.b2)
+        return I # Current [A]
 
-    def current_to_pwm(I):
-        return B
 
-    def field_to_pwm(PWM):
-        return B
-    """
+    def current_to_pwm(self, I):
+        PWM = np.linalg.inv(self.M1) @ (I - self.b1)
+        return PWM # Duty Cycle [%]
+
+    def field_to_pwm(self, B):
+        PWM = np.linalg.inv(self.M3) @ (B - self.b3)
+        return PWM # Duty Cycle [%]
         
 
 if __name__ == '__main__':
     coils = Coils()
-    x = [10, 50, 20, 80, 30, 60]
-    coils.pwm_to_current(np.array(x))
+    PWM = [10, 50, 20, 80, 30, 100]
+    I = coils.pwm_to_current(np.array(PWM))
+    B = coils.current_to_field(np.array(I))
+
+    print(coils.M2)
+
+
+
