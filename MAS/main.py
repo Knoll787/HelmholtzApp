dd7f8032885f372fd52ee87bf22748785a5c3f02
knoll787 <noel.woods@protonmail.com>
2025-09-16 13:51:05 +1000

    Working on PID tuning app. Added markers and system arguments

--- MAS/main.py
+++ MAS/main.py
@@ -1,6 +1,14 @@
+import argparse
 import image_processing as ip 
 import movement as mv
 import controllers as ctlr
+
+
+parser = argparse.ArgumentParser(description="PID Tuning Application")
+parser.add_argument("sim_time", type=float, help="Time that the simulation will run for")
+parser.add_argument("x_target", type=int, default=142, help="X position to move towards")
+parser.add_argument("y_target", type=int, default=137, help="Y position to move towards")
+args = parser.parse_args()
 
 camera = ip.PiCamera()
 ret, first_frame = camera.read()
@@ -24,6 +32,8 @@ try:
         x.set_magnetic_field(ctl_x.compute(pos[0])) 
         y.set_magnetic_field(ctl_y.compute(pos[1])) 
 
+        ip.cv2.circle(frame, (args.x_target, args.x_target), radius=5, color=(0, 0, 255), thickness=1)
+        ip.cv2.circle(frame, pos, radius=5, color=(255, 0, 0), thickness=1)
         ip.cv2.imshow("Camera Feed", frame)
 
         # Exit on ESC
